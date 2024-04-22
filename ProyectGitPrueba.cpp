5977298c1198e6917955e942ce4d93182c6b29e2
Elvis Alejandro Lopez Gambini <135379186+ElvisLG06@users.noreply.github.com>
2024-04-22 17:18:45 -0500

    Primer commit

---
+++ ProyectGitPrueba.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Git" << endl;
+
+    int num1 = 5, num2 = 3;
+
+    int total = num1 + num2;
+
+    cout << "La suma es: " << total << endl;
+
+    return 0;
+}
