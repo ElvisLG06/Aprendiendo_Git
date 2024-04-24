8c96be9a92364643084901773eb8a5483231f3a3
Elvis Alejandro Lopez Gambini <135379186+ElvisLG06@users.noreply.github.com>
2024-04-24 17:33:31 -0500

    Solucion correcta, No revisar (#2)
    Funciona

--- ProyectGitPrueba.cpp
+++ ProyectGitPrueba.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello Git" << endl;
+    cout << "Hello Git Issue" << endl;
 
     int num1 = 5, num2 = 3;
 
     int total = num1 + num2;
 
     cout << "La suma es: " << total << endl;
+
+    cout << "Adios Git Issue" << endl;
 
     return 0;
 }
